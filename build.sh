6d5c04afacdb3871d000b7fc964f36ee2d914088
Bruce Flynn <brucef@ssec.wisc.edu>
2024-01-29 15:12:05 -0600

    make version optionsl

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 
 database=spacecraftsdb.json
 gitsha=$(git rev-parse HEAD)
-version=$(git describe --match '*.*.*')
+version=$(git describe --match '*.*.*' || true)
 generated=$(date -uIseconds)
 
 jq "{\"spacecrafts\": ., \"version\": \"${version}\", \"gitSha\": \"${gitsha}\", \"generated\":\"${generated}\"}" spacecrafts.data.json > ${database}
