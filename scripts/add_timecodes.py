c6dc4335cc6ce3b9d7d5149be4d5593f354272af
Bruce Flynn <brucef@ssec.wisc.edu>
2024-11-06 21:40:48 -0600

    add links; change timecode field names

--- scripts/add_timecodes.py
+++ scripts/add_timecodes.py
@@ -15,20 +15,20 @@ for sc in [s for s in spacecrafts if s["scid"] in (157, 159, 177, 178, 179)]:
             if apid["apid"] in [515, 528, 530, 531] + list(range(800, 829)) + list(
                 range(1315, 1396)
             ) + [1289, 1290, 0, 8, 11, 30, 34, 37]:
-                apid["timecodeFormat"] = {"format": "cds"}
+                apid["timecodeFormat"] = {"epoch": "1958-01-01T00:00:00Z", "format": "cds", "offset": 0, "numDay": 2, "numSubmillis": 2}
 
 # MODIS science data gets CDS
 for sc in [s for s in spacecrafts if s["scid"] in [64, 154]]:
     for vcid in sc["vcids"]:
         for apid in vcid["apids"]:
             if apid["apid"] in [64]:
-                apid["timecodeFormat"] = {"format": "cds"}
+                apid["timecodeFormat"] = {"epoch": "1958-01-01T00:00:00Z", "format": "cds", "offset": 0, "numDay": 2, "numSubmillis": 2}
 
 # GBAD gets CUC at byte offset 16, skipping p-field
 for sc in [s for s in spacecrafts if s["scid"] in [154]]:
     for vcid in sc["vcids"]:
         for apid in vcid["apids"]:
             if apid["apid"] in [957]:
-                apid["timecodeFormat"] = {"format": "cuc", "offset": 16}
+                apid["timecodeFormat"] = {"epoch": "1958-01-01T00:00:00Z", "format": "cuc", "offset": 2, "numCoarse": 2, "numFine": 4, "fineMult": 15200.0}
 
 json.dump(spacecrafts, sys.stdout, indent=2)
